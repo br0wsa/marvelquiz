4c6d04a639917a9aa11a36f3dc5d20c27d4058db
git504 <dngpoje6@duck.com>
2023-02-07 10:44:52 +0100

    feat: :sparkles: Les évènements onMouseOver et onMouseOut

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 const Landing = () => {
-  return <main className="welcomePage"></main>;
+  const refWolverine = useRef(null);
+  const [btn, setBtn] = useState(false);
+
+  useEffect(() => {
+    refWolverine.current.classList.add("startingImg");
+
+    setTimeout(() => {
+      setBtn(true);
+      refWolverine.current.classList.remove("startingImg");
+    }, 1000);
+  }, []);
+
+  const setLeftImage = () => {
+    refWolverine.current.classList.add("leftImg");
+  };
+
+  const setRightImage = () => {
+    refWolverine.current.classList.add("rightImg");
+  };
+
+  const clearImage = () => {
+    if (refWolverine.current.classList.contains("leftImg")) {
+      refWolverine.current.classList.remove("leftImg");
+    } else if (refWolverine.current.classList.contains("rightImg")) {
+      refWolverine.current.classList.remove("rightImg");
+    }
+  };
+
+  const displayBtn = btn && (
+    <>
+      <div
+        onMouseOver={setLeftImage}
+        onMouseOut={clearImage}
+        className="leftBox"
+      >
+        <button className="btn-welcome">Inscription</button>
+      </div>
+      <div
+        onMouseOver={setRightImage}
+        onMouseOut={clearImage}
+        className="rightBox"
+      >
+        <button className="btn-welcome">Connection</button>
+      </div>
+    </>
+  );
+
+  return (
+    <main ref={refWolverine} className="welcomePage">
+      {displayBtn}
+    </main>
+  );
 };
 
 export default Landing;
