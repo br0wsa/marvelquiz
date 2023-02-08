f771fb99bd19dd31b337eb6e16eeea75aa6f44b5
git504 <dngpoje6@duck.com>
2023-02-08 16:17:40 +0100

    feat: :sparkles: Gestion de la session utilisateur via Firebase - Firebase 8

--- src/components/Logout/index.jsx
+++ src/components/Logout/index.jsx
@@ -1,16 +1,22 @@
 import React, { useState, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { FirebaseContext } from "../Firebase";
 
 const Logout = () => {
   const firebase = useContext(FirebaseContext);
   const [checked, setChecked] = useState(false);
-  
+  const navigate = useNavigate();
+
   useEffect(() => {
     if (checked) {
-      console.log("déconnexion");
-      firebase.signoutUser();
+      setTimeout(() => {
+        console.log("déconnexion");
+        navigate("/");
+        firebase.signoutUser();
+      }, 1000);
     }
-  }, [checked, firebase]);
+  }, [checked, firebase, navigate]);
 
   const handleChange = (e) => {
     setChecked(e.target.checked);
--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import Logout from "../Logout";
 import Quiz from "../Quiz";
 
+import { FirebaseContext } from "../Firebase";
+
 const Welcome = () => {
-  return (
+  const firebase = useContext(FirebaseContext);
+  const navigate = useNavigate();
+  const [userSession, setUserSession] = useState(null);
+
+  useEffect(() => {
+    let listener = firebase.auth.onAuthStateChanged((user) => {
+      user ? setUserSession(user) : navigate("/");
+    });
+
+    return () => {
+      listener();
+    };
+  }, []);
+
+  return userSession === null ? (
+    <>
+      <div className="loader">
+        <p></p>
+      </div>
+    </>
+  ) : (
     <div className="quiz-bg">
       <div className="container">
         <Logout />
