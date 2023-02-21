377c2460a44f0179db7cc01f49c97c33828bd344
git504 <dngpoje6@duck.com>
2023-02-21 16:11:07 +0100

    feat: :construction: MARVEL QUIZ
    Composant Quiz, Levels et Progressbar

--- src/components/Quiz/index.jsx
+++ src/components/Quiz/index.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Levels from "../Levels";
+import ProgessBar from "../ProgressBar";
 
 class Quiz extends Component {
   render() {
@@ -6,7 +8,14 @@ class Quiz extends Component {
 
     return (
       <div>
-        <h2>PSEUDO : {pseudo}</h2>
+        <Levels />
+        <ProgessBar />
+        <h2>Notre question Quiz</h2>
+        <p className="answerOptions">Question 1</p>
+        <p className="answerOptions">Question 2</p>
+        <p className="answerOptions">Question 3</p>
+        <p className="answerOptions">Question 4</p>
+        <button className="btnSubmit">Suivant</button>
       </div>
     );
   }
